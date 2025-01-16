06bf6d8ccadcbdde5b49cfbc1238fc8f475a09d1
Genevieve Dufour <genevieve.dufour@unity3d.com>
2025-01-16 10:33:59 -0500

    Add tests for baseball game

---
+++ TestLeetCode/Gen/BaseballGameTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using LeetCode.Gen;
+using NUnit.Framework;
+using Sandbox;
+
+namespace TestLeetCode.Gen;
+
+public class BaseballGameTests
+{
+    static IEnumerable<TestCaseData> TestCases()
+    {
+        yield return new TestCaseData([(string[]) ["1", "C"]]).Returns(0).SetName("example1");
+        yield return new TestCaseData([(string[]) ["5", "2", "C", "D", "+"]]).Returns(30).SetName("example2");
+        yield return new TestCaseData([(string[]) ["5", "-2", "4", "C", "D", "9", "+", "+"]]).Returns(27).SetName("example3");
+        yield return new TestCaseData([(string[]) ["1","C"]]).Returns(0).SetName("example4");
+    }
+
+    [Test, TestCaseSource(nameof(TestCases))]
+    public int TestSolution(string[] operations)
+    {
+        var testObject = new BaseballGame682();
+        return MeasureExecutionTime.Measure(() => testObject.Solution(operations));
+    }
+}
