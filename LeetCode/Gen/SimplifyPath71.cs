3ae354c4997be0a40b3eb22589b253f8854c558b
Genevieve Dufour <genevieve.dufour@unity3d.com>
2025-01-16 10:15:16 -0500

    Change order of conditions

--- LeetCode/Gen/SimplifyPath71.cs
+++ LeetCode/Gen/SimplifyPath71.cs
@@ -11,27 +11,18 @@ public class SimplifyPath71
         return SimplifyPath(path);
     }
 
-    /*
-        yield return new TestCaseData("/").Returns("/").SetName("SingleSlash");
-        yield return new TestCaseData("/home/").Returns("/home").SetName("example1");
-        yield return new TestCaseData("/home//foo/").Returns("/home/foo").SetName("example2");
-        yield return new TestCaseData("/home/user/Documents/../Pictures").Returns("/home/user/Pictures").SetName("example3");
-        yield return new TestCaseData("/../").Returns("/").SetName("example4");
-        yield return new TestCaseData("/.../a/../b/c/../d/./").Returns("/.../b/d").SetName("example5");
-     */
-
     static string SimplifyPath(string path)
     {
         var stack = new Stack<string>();
-        string[] words = path.Split('/');
-        int skipNextWord = 0;
+        var words = path.Split('/');
+        var skipNextWord = 0;
 
         for(int i = words.Length - 1; i >= 0; i--)
         {
+            if (words[i] == "." || words[i] == string.Empty)
+                continue;
             if (words[i] == "..")
                 skipNextWord++;
-            else if (words[i] == "." || words[i] == string.Empty)
-                continue;
             else
             {
                 if (skipNextWord > 0)
