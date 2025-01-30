bc62dd39d5eb6caac284fdb64707ec6eb112f50e
Genevieve Dufour <genevieve.dufour@unity3d.com>
2025-01-30 10:24:59 -0500

    Valid palindrome 125

---
+++ LeetCode/Gen/ValidPalindrome125.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace LeetCode.Gen;
+
+//https://leetcode.com/problems/valid-palindrome/description/
+//125. Valid Palindrome
+
+public class ValidPalindrome125
+{
+    public bool IsPalindrome(string s)
+    {
+        var characters = s.ToCharArray();
+        var l = 0;
+        var r = characters.Length - 1;
+
+        while (l <= r)
+        {
+            var leftChar = char.ToLower(characters[l]);
+            var rightChar = char.ToLower(characters[r]);
+
+            if (!char.IsAsciiLetterOrDigit(leftChar))
+            {
+                l++;
+                continue;
+            }
+            if (!char.IsAsciiLetterOrDigit(rightChar))
+            {
+                r--;
+                continue;
+            }
+
+            if (leftChar == rightChar)
+            {
+                l++;
+                r--;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ TestLeetCode/Gen/ValidPalindromeTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using LeetCode.Gen;
+using NUnit.Framework;
+using Sandbox;
+
+namespace TestLeetCode.Gen;
+public class ValidPalindromeTests
+{
+    static IEnumerable<TestCaseData> TestCases()
+    {
+        yield return new TestCaseData("A man, a plan, a canal: Panama").Returns(true).SetName("valid palindrome");
+        yield return new TestCaseData("race a car").Returns(false).SetName("not a palindrome");
+        yield return new TestCaseData(" ").Returns(true).SetName("emptyString");
+        yield return new TestCaseData("0P").Returns(false).SetName("0P");
+
+    }
+
+    [Test, TestCaseSource(nameof(TestCases))]
+    public bool TestSolution(string s)
+    {
+        var testObject = new ValidPalindrome125();
+        return MeasureExecutionTime.Measure(() => testObject.IsPalindrome(s));
+    }
+}
