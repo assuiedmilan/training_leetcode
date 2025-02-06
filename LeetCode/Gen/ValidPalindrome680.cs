a13d46a854c55522b7476ca6656a32a567184e0d
Genevieve Dufour <genevieve.dufour@unity3d.com>
2025-02-06 10:16:20 -0500

    Valid palindrome

---
+++ LeetCode/Gen/ValidPalindrome680.cs
@@ -0,0 +1,46 @@
+﻿namespace LeetCode.Gen;
+
+// https://leetcode.com/problems/valid-palindrome-ii/description/
+// 680. Valid Palindrome II
+
+public class ValidPalindrome680
+{
+    public bool ValidPalindrome(string s)
+    {
+        var l = 0;
+        var r = s.Length - 1;
+
+        while (l <= r)
+        {
+            if (s[l] == s[r])
+            {
+                l++;
+                r--;
+            }
+            else
+            {
+                return IsPalindrome(s, l + 1, r) || IsPalindrome(s, l, r - 1);
+            }
+        }
+
+        return true;
+    }
+
+    bool IsPalindrome(string s, int l, int r)
+    {
+        while (l <= r)
+        {
+            if (s[l] == s[r])
+            {
+                l++;
+                r--;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ TestLeetCode/Gen/ValidPalindrome2Tests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using LeetCode.Gen;
+using NUnit.Framework;
+using Sandbox;
+
+namespace TestLeetCode.Gen;
+public class ValidPalindrome2Tests
+{
+    static IEnumerable<TestCaseData> TestCases()
+    {
+        yield return new TestCaseData("aba").Returns(true).SetName("valid palindrome");
+        yield return new TestCaseData("abca").Returns(true).SetName("delete1");
+        yield return new TestCaseData("abc").Returns(false).SetName("not valid");
+        yield return new TestCaseData("cbbcc").Returns(true).SetName("valid");
+        yield return new TestCaseData("aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga").Returns(true).SetName("valid2");
+        yield return new TestCaseData("lcuppucul").Returns(true).SetName("valid3");
+    }
+
+    [Test, TestCaseSource(nameof(TestCases))]
+    public bool TestSolution(string s)
+    {
+        var testObject = new ValidPalindrome680();
+        return MeasureExecutionTime.Measure(() => testObject.ValidPalindrome(s));
+    }
+}
