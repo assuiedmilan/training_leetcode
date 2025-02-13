be4d4b1eec7b501275baf448b5e96b63d1d42bb5
Genevieve Dufour <genevieve.dufour@unity3d.com>
2025-02-13 11:01:29 -0500

    Skelette Remove Duplicates

---
+++ LeetCode/Gen/RemoveDuplicatesFromSortedArray26.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCode.Gen;
+
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+// 26. Remove Duplicates from Sorted Array
+
+public class RemoveDuplicatesFromSortedArray26
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        return -1;
+    }
+}
---
+++ TestLeetCode/Gen/RemoveDuplicatesFromSortedArray26Tests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using LeetCode.Gen;
+using NUnit.Framework;
+using Sandbox;
+
+public class RemoveDuplicatesFromSortedArray26Tests
+{
+    static IEnumerable<TestCaseData> TestCases()
+    {
+        yield return new TestCaseData((int[])[1,1,2]).Returns(2).SetName("Example 1");
+        yield return new TestCaseData((int[])[0,0,1,1,1,2,2,3,3,4]).Returns(5).SetName("Example 2");
+    }
+
+    [Test, TestCaseSource(nameof(TestCases))]
+    public int TestSolution(int[] nums)
+    {
+        var testObject = new RemoveDuplicatesFromSortedArray26();
+        return MeasureExecutionTime.Measure(() => testObject.RemoveDuplicates(nums));
+    }
+}
