4268ecbcd31e0fe6520a1f60020b5f78d27005eb
Genevieve Dufour <genevieve.dufour@unity3d.com>
2025-02-13 09:17:40 -0500

    Merge Alternately

---
+++ LeetCode/Gen/MergeStringsAlternately1768.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LeetCode.Gen;
+
+// https://leetcode.com/problems/merge-strings-alternately/
+// 1768. Merge Strings Alternately
+
+public class MergeStringsAlternately1768
+{
+    public string MergeAlternately(string word1, string word2)
+    {
+        var result = string.Empty;
+        var stringLength = Math.Min(word1.Length, word2.Length);
+
+        if (word1.Length == word2.Length)
+        {
+            stringLength = word1.Length;
+        }
+
+        for (int i = 0; i < stringLength; i++)
+        {
+            result += word1[i] + "" + word2[i];
+        }
+
+        if(word1.Length > word2.Length)
+        {
+            result += word1.Substring(stringLength, word1.Length - stringLength);
+        }
+        else if(word2.Length > word1.Length)
+        {
+            result += word2.Substring(stringLength, word2.Length - stringLength);
+        }
+
+        return result;
+    }
+}
---
+++ TestLeetCode/Gen/MergeStringsAlternately1768Tests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using LeetCode.Gen;
+using NUnit.Framework;
+using Sandbox;
+
+namespace TestLeetCode.Gen;
+public class MergeStringsAlternately1768Tests
+{
+    static IEnumerable<TestCaseData> TestCases()
+    {
+        yield return new TestCaseData("abc", "pqr").Returns("apbqcr").SetName("Example 1");
+        yield return new TestCaseData("ab", "pqrs").Returns("apbqrs").SetName("Example 2");
+        yield return new TestCaseData("abcd", "pq").Returns("apbqcd").SetName("Example 3");
+        yield return new TestCaseData("cdf", "a").Returns("cadf").SetName("Example 4");
+    }
+
+    [Test, TestCaseSource(nameof(TestCases))]
+    public string TestSolution(string word1, string word2)
+    {
+        var testObject = new MergeStringsAlternately1768();
+        return MeasureExecutionTime.Measure(() => testObject.MergeAlternately(word1, word2));
+    }
+}
