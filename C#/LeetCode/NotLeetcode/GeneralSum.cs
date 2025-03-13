c6063078fb6d167f08a1d4cbbe0a6420ffd39e62
Milan Assuied <milan.assuied@unity3d.com>
2025-03-13 09:50:38 -0400

    GeneralSum

---
+++ C#/LeetCode/NotLeetcode/GeneralSum.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.NotLeetcode;
+
+
+//N-Sum based on TwoSumII, ThreeSum and FourSum problems
+public class GeneralSum
+{
+    public IList<IList<int>> Solution(int[] nums, int target, int combinationSize)
+    {
+        List<IList<int>> ans = [];
+
+        if (nums.Length < combinationSize) return ans;
+
+        Array.Sort(nums);
+        
+        if (combinationSize == 2)
+        {
+            TwoSum(nums, target, ans);
+        }
+
+        return ans;
+    }
+
+    void TwoSum(int[] nums, int target, IList<IList<int>> result)
+    {
+        Array.Sort(nums);
+        var left = 0;
+        var right = nums.Length - 1;
+
+        while (left < right)
+        {
+            var current = nums[left] + nums[right];
+
+            if (current == target)
+            {
+                result.Add([nums[left], nums[right]]);
+                while (left < right && nums[left] == nums[left + 1]) left++;
+                while (left < right && nums[right] == nums[right - 1]) right--;
+                left++;
+                right--;
+                continue;
+            }
+            
+            if (current < target)
+            {
+                left++;
+                continue;
+            }
+
+            right--;
+        }
+    }
+}
---
+++ C#/TestLeetCode/NotLeetcode/TestGeneralSum.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using LeetCode.NotLeetcode;
+using NUnit.Framework;
+
+namespace TestLeetCode.NotLeetcode;
+
+public class TestGeneralSum
+{
+    static IEnumerable<TestCaseData> TestCases()
+    {
+        yield return new TestCaseData((int[]) [1, 2, 3, 4, 5], 5, 2).Returns((IList<int[]>) [[1, 4], [2, 3]]).SetName("MultipleCombinations");
+        yield return new TestCaseData((int[]) [3, 1, 4, 1, 5], 6, 2).Returns((IList<int[]>) [[1, 5]]).SetName("SingleCombination");
+        yield return new TestCaseData((int[]) [1, 1, 1, 1, 1], 2, 2).Returns((IList<int[]>) [[1, 1]]).SetName("AllSameValues");
+        yield return new TestCaseData((int[]) [], 0, 2).Returns((IList<int[]>) []).SetName("EmptyArray");
+        yield return new TestCaseData((int[]) [1], 1, 2).Returns((IList<int[]>) []).SetName("SingleElementArray");
+    }
+
+    [Test, TestCaseSource(nameof(TestCases))]
+    public IList<IList<int>> TestSolution(int[] array, int target, int combinationSize)
+    {
+        var testObject = new GeneralSum();
+        return testObject.Solution(array, target, combinationSize);
+    }
+}
