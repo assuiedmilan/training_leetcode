4beec480815e4e41864287b1b8954ade29a2848b
Genevieve Dufour <genevieve.dufour@unity3d.com>
2025-04-10 09:50:34 -0400

    Container Water 11

---
+++ LeetCode/Gen/ContainerWater11.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace LeetCode.Gen;
+
+/*
+ * https://leetcode.com/problems/container-with-most-water/
+ * 11. Container With Most Water
+ */
+
+/*
+ * You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+Return the maximum amount of water a container can store.
+
+Notice that you may not slant the container.
+ */
+
+public class ContainerWater11
+{
+  public int MaxArea(int[] height)
+  {
+      var maxArea = 0;
+      var left = 0;
+      var right = height.Length - 1;
+
+      while (left <= right)
+      {
+          var area = Math.Abs(left - right) * Math.Min(height[left], height[right]);
+          maxArea = Math.Max(area, maxArea);
+          if (height[left] < height[right])
+          {
+              left++;
+          }
+          else
+          {
+              right--;
+          }
+      }
+      return maxArea;
+  }
+}
---
+++ TestLeetCode/Gen/ContainerWater11Tests.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using LeetCode.Gen;
+using NUnit.Framework;
+using Sandbox;
+
+namespace TestLeetCode.Gen;
+
+public class ContainerWater11Tests
+{
+    static IEnumerable<TestCaseData> TestCases()
+    {
+        yield return new TestCaseData((int[])[1,8,6,2,5,4,8,3,7]).Returns(49).SetName("Example 1");
+        yield return new TestCaseData((int[])[1,1]).Returns(1).SetName("Example 2");
+    }
+
+    [Test, TestCaseSource(nameof(TestCases))]
+    public int TestSolution(int[] height)
+    {
+        var testObject = new ContainerWater11();
+        return MeasureExecutionTime.Measure(() => testObject.MaxArea(height));
+    }
+}
