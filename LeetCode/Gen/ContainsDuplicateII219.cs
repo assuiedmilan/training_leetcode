b765163ea220aebf787f75ac85c22444507d7a0e
Genevieve Dufour <genevieve.dufour@unity3d.com>
2025-04-17 10:21:35 -0400

    ContainsDuplicateII219

---
+++ LeetCode/Gen/ContainsDuplicateII219.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Gen;
+
+/*
+ * https://leetcode.com/problems/contains-duplicate-ii/
+ * 219. Contains Duplicate II
+ */
+
+/*
+ * Given an integer array nums and an integer k, return true if there are two distinct indices i and j
+ * in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+ */
+
+
+// Input: nums = [1,2,3,1], k = 3
+// Output: true
+
+public class ContainsDuplicateII219
+{
+    public bool ContainsNearbyDuplicate(int[] nums, int k)
+    {
+        var dict = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (!dict.TryAdd(nums[i], i))
+            {
+                var duplicate = dict[nums[i]];
+                if (i - duplicate <= k)
+                {
+                    return true;
+                }
+
+                dict[nums[i]] = i;
+            }
+        }
+
+        return false;
+    }
+
+  // public bool ContainsNearbyDuplicate(int[] nums, int k)
+  // {
+  //     for (int i = 0; i < nums.Length-1; i++)
+  //     {
+  //         for (int j = i + 1; j <= i+k; j++)
+  //         {
+  //             if (j > nums.Length - 1)
+  //                 break;
+  //
+  //             if (nums[i] == nums[j])
+  //             {
+  //                 return true;
+  //             }
+  //         }
+  //     }
+  //
+  //     return false;
+  // }
+}
---
+++ TestLeetCode/Gen/ContainsDuplicateII219Tests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using LeetCode.Gen;
+using NUnit.Framework;
+using Sandbox;
+
+namespace TestLeetCode.Gen;
+
+public class ContainsDuplicateII219Tests
+{
+    static IEnumerable<TestCaseData> TestCases()
+    {
+        yield return new TestCaseData((int[])[1,2,3,1], 3).Returns(true).SetName("Example 1");
+        yield return new TestCaseData((int[])[1,0,1,1], 1).Returns(true).SetName("Example 2");
+        yield return new TestCaseData((int[])[1,2,3,1,2,3], 2).Returns(false).SetName("Example 3");
+    }
+
+    [Test, TestCaseSource(nameof(TestCases))]
+    public bool TestSolution(int[] nums, int k)
+    {
+        var testObject = new ContainsDuplicateII219();
+        return MeasureExecutionTime.Measure(() => testObject.ContainsNearbyDuplicate(nums, k));
+    }
+}
