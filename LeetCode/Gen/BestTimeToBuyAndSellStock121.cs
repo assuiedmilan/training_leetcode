a59556b7ce8d37fcb27aa1e1ed0cf15fe8cf56da
Genevieve Dufour <genevieve.dufour@unity3d.com>
2025-04-17 10:56:39 -0400

    Best Time to Buy and Sell Stock

---
+++ LeetCode/Gen/BestTimeToBuyAndSellStock121.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode.Gen;
+
+public class BestTimeToBuyAndSellStock121
+{
+    /*
+     * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+     * 121. Best Time to Buy and Sell Stock
+     */
+
+    /*
+     * You are given an array prices where prices[i] is the price of a given stock on the ith day.
+     * You want to maximize your profit by choosing a single day to buy one stock and choosing a different day
+     * in the future to sell that stock.
+     * Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+     */
+
+    public int MaxProfit(int[] prices)
+    {
+        var maxProfit = 0;
+        var minPrice = int.MaxValue;
+
+        for (int i = 0; i < prices.Length; i++)
+        {
+            if (prices[i] < minPrice)
+            {
+                minPrice = prices[i];
+            }
+            else if(prices[i] - minPrice > maxProfit)
+            {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+
+        return maxProfit;
+    }
+}
---
+++ TestLeetCode/Gen/BestTimeToBuyAndSellStock121Tests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using LeetCode.Gen;
+using NUnit.Framework;
+using Sandbox;
+
+namespace TestLeetCode.Gen;
+
+public class BestTimeToBuyAndSellStock121Tests
+{
+    static IEnumerable<TestCaseData> TestCases()
+    {
+        yield return new TestCaseData((int[])[7,1,5,3,6,4]).Returns(5).SetName("Example 1");
+        yield return new TestCaseData((int[])[7,6,4,3,1]).Returns(0).SetName("Example 2");
+        yield return new TestCaseData((int[])[10,1,5,6,7,1]).Returns(6).SetName("Example 3");
+        yield return new TestCaseData((int[])[10,8,7,5,2]).Returns(0).SetName("Example 4");
+        yield return new TestCaseData((int[])[1,2]).Returns(1).SetName("Example 5");
+    }
+
+    [Test, TestCaseSource(nameof(TestCases))]
+    public int TestSolution(int[] prices)
+    {
+        var testObject = new BestTimeToBuyAndSellStock121();
+        return MeasureExecutionTime.Measure(() => testObject.MaxProfit(prices));
+    }
+}
