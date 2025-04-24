c88af073c26a5e7790f1a2ac5bb4933c1d1e8f10
Genevieve Dufour <genevieve.dufour@unity3d.com>
2025-04-24 10:26:29 -0400

    LongestSubstringWithoutRepeatingCharacters3

---
+++ LeetCode/Gen/LongestSubstringWithoutRepeatingCharacters3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Gen;
+
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+// 3. Longest Substring Without Repeating Characters
+
+public class LongestSubstringWithoutRepeatingCharacters3
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        if (s.Length == 0) return 0;
+        if (s.Length == 1) return 1;
+
+        var hashSet = new HashSet<char>();
+        var left = 0;
+        var maxCount = 0;
+
+        for (int right = 0; right < s.Length; right++)
+        {
+            while (hashSet.Contains(s[right]))
+            {
+                hashSet.Remove(s[left++]);
+            }
+
+            hashSet.Add(s[right]);
+            maxCount = Math.Max(maxCount, right - left + 1);
+        }
+
+        return maxCount;
+    }
+}
---
+++ TestLeetCode/Gen/LongestSubstringWithoutRepeatingCharacters3Tests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using LeetCode.Gen;
+using NUnit.Framework;
+using Sandbox;
+
+namespace TestLeetCode.Gen;
+
+public class LongestSubstringWithoutRepeatingCharacters3Tests
+{
+    static IEnumerable<TestCaseData> TestCases()
+    {
+        yield return new TestCaseData("abcabcbb").Returns(3).SetName("Example 1");
+        yield return new TestCaseData("bbbbb").Returns(1).SetName("Example 2");
+        yield return new TestCaseData("pwwkew").Returns(3).SetName("Example 3");
+        yield return new TestCaseData("aa").Returns(1).SetName("Example 4");
+        yield return new TestCaseData("aab").Returns(2).SetName("Example 5");
+    }
+
+    [Test, TestCaseSource(nameof(TestCases))]
+    public int TestSolution(string s)
+    {
+        var testObject = new LongestSubstringWithoutRepeatingCharacters3();
+        return MeasureExecutionTime.Measure(() => testObject.LengthOfLongestSubstring(s));
+    }
+}
