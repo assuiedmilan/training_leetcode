3ab87070598d1c200ab8aaca42ef1d9baf7d07f2
Genevieve Dufour <genevieve.dufour@unity3d.com>
2025-04-24 10:28:54 -0400

    LongestRepeatingCharacterReplacement424

---
+++ LeetCode/Gen/LongestRepeatingCharacterReplacement424.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCode.Gen;
+
+// https://leetcode.com/problems/longest-repeating-character-replacement/
+// 424. Longest Repeating Character Replacement
+
+public class LongestRepeatingCharacterReplacement424
+{
+    public int CharacterReplacement(string s, int k)
+    {
+        return 0;
+    }
+}
---
+++ TestLeetCode/Gen/LongestRepeatingCharacterReplacement424Tests.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using LeetCode.Gen;
+using NUnit.Framework;
+using Sandbox;
+
+namespace TestLeetCode.Gen;
+
+public class LongestRepeatingCharacterReplacement424Tests
+{
+    static IEnumerable<TestCaseData> TestCases()
+    {
+        yield return new TestCaseData("ABAB", 2).Returns(4).SetName("Example 1");
+        yield return new TestCaseData("AABABBA", 1).Returns(4).SetName("Example 2");
+    }
+
+    [Test, TestCaseSource(nameof(TestCases))]
+    public int TestSolution(string s, int k)
+    {
+        var testObject = new LongestRepeatingCharacterReplacement424();
+        return MeasureExecutionTime.Measure(() => testObject.CharacterReplacement(s, k));
+    }
+}
