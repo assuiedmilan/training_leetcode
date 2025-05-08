8c114191a486c77e61d14a526a076fcd7037d4e3
Genevieve Dufour <genevieve.dufour@unity3d.com>
2025-05-08 10:23:21 -0400

    meh

---
+++ LeetCode/Gen/PermutationInString567.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace LeetCode.Gen;
+
+// https://leetcode.com/problems/permutation-in-string/
+// 424. Longest Repeating Character Replacement
+
+/*
+ * Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+ * In other words, return true if one of s1's permutations is the substring of s2.
+ *
+ * Example 1:
+ * Input: s1 = "ab", s2 = "eidbaooo"
+ * Output: true
+ * Explanation: s2 contains one permutation of s1 ("ba").
+ *
+ * Example 2:
+ * Input: s1 = "ab", s2 = "eidboaoo"
+ * Output: false
+ *
+ * Constraints:
+ * 1 <= s1.length, s2.length <= 104
+ * s1 and s2 consist of lowercase English letters.
+ */
+
+public class PermutationInString567
+{
+    public bool CheckInclusion(string s1, string s2)
+    {
+        var result = false;
+        var left = 0;
+        var right = s1.Length - 1;
+
+
+
+
+        return result;
+    }
+}
---
+++ TestLeetCode/Gen/PermutationInString567Tests.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using LeetCode.Gen;
+using NUnit.Framework;
+using Sandbox;
+
+namespace TestLeetCode.Gen;
+
+public class PermutationInString567Tests
+{
+    static IEnumerable<TestCaseData> TestCases()
+    {
+        yield return new TestCaseData("ab", "eidbaooo").Returns(true).SetName("Example 1");
+        yield return new TestCaseData("ab", "s2").Returns(false).SetName("Example 2");
+    }
+
+    [Test, TestCaseSource(nameof(TestCases))]
+    public bool TestSolution(string s1, string s2)
+    {
+        var testObject = new PermutationInString567();
+        return MeasureExecutionTime.Measure(() => testObject.CheckInclusion(s1, s2));
+    }
+}
