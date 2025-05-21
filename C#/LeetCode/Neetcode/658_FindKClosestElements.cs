d62594d50ac847219408799dd26c2781cafd47fe
Milan Assuied <milan.assuied@unity3d.com>
2025-05-21 11:30:44 -0400

    KClosestElements

---
+++ C#/LeetCode/Neetcode/658_FindKClosestElements.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Neetcode;
+
+/* https://leetcode.com/problems/find-k-closest-elements/ */
+public class FindKClosestElements
+{
+    public IList<int> Solution(int[] arr, int k, int x)
+    {
+        if (k == arr.Length || arr.Length == 1) return arr;
+
+        #region Find index of x
+
+        var left = 0;
+        var right = arr.Length - 1;
+        var indexOfX =0;
+        
+        while (left <= right)
+        {
+            indexOfX = left + (right - left) / 2;
+            if (arr[indexOfX] == x) break;
+            if (arr[indexOfX] < x) left = indexOfX + 1; 
+            else right = indexOfX - 1; 
+        }
+
+        if (arr[indexOfX] != x)
+        {
+            if (left >= arr.Length) indexOfX = right;
+            else if (right < 0) indexOfX = left;
+            else indexOfX = Math.Abs(arr[left] - x) < Math.Abs(arr[right] - x) ? left : right;
+        }
+        #endregion
+        
+        #region Find closests elements
+
+        var leftPart = new List<int> { arr[indexOfX] };
+        var rightPart = new List<int>();
+        
+        left = indexOfX - 1;
+        right = indexOfX + 1;
+        k--;
+
+        while ( k != 0)
+        {
+            switch (left)
+            {
+                case > -1 when right == arr.Length:
+                case > -1 when (x - arr[left] < arr[right] - x || x - arr[left] == arr[right] - x):
+                    leftPart.Add(arr[left]);
+                    left--;
+                    k--;
+                    continue;
+            }
+
+            if (right < arr.Length)
+            {
+                rightPart.Add(arr[right]);
+                right++;
+                k--;
+                continue;
+            }
+
+            break;
+        }
+
+        var res = new List<int>();
+
+        for (var i = leftPart.Count - 1; i > -1; i--)
+        {
+            res.Add(leftPart[i]);
+        }
+
+        foreach (var t in rightPart)
+        {
+            res.Add(t);
+        }
+        
+        #endregion
+        return res;
+
+    }
+}
---
+++ C#/TestLeetCode/658_TestFindKClosestElements.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using LeetCode.Neetcode;
+using NUnit.Framework;
+
+namespace TestLeetCode;
+
+public class TestFindKClosestElements
+{
+    static IEnumerable<TestCaseData> TestCases()
+    {
+        yield return new TestCaseData((int[]) [1, 2, 3, 4, 5], 4, 3)
+            .Returns((int[]) [1, 2, 3, 4])
+            .SetName("KLessThanArrayLength_TargetInMiddle");
+        yield return new TestCaseData((int[]) [1, 2, 3, 4, 5], 2, 3)
+            .Returns((int[]) [2, 3])
+            .SetName("KLessThanArrayLength_TargetInMiddle_SmallerK");
+        yield return new TestCaseData((int[]) [1, 2, 3, 4, 5], 5, 3)
+            .Returns((int[]) [1, 2, 3, 4, 5])
+            .SetName("KEqualsArrayLength");
+        yield return new TestCaseData((int[]) [1, 2, 3, 4, 5], 3, 6)
+            .Returns((int[]) [3, 4, 5])
+            .SetName("TargetGreaterThanAllElements");
+        yield return new TestCaseData((int[]) [1, 2, 3, 4, 5], 3, -1)
+            .Returns((int[]) [1, 2, 3])
+            .SetName("TargetLessThanAllElements");
+        yield return new TestCaseData((int[]) [1, 1, 1, 10, 10, 10], 3, 9)
+            .Returns((int[]) [10, 10, 10])
+            .SetName("AllCloseToTarget");
+    }
+
+    [Test, TestCaseSource(nameof(TestCases))]
+    public int[] TestSolution(int[] arr, int k, int x)
+    {
+        var testObject = new FindKClosestElements();
+        return testObject.Solution(arr, k, x).ToArray();
+    }
+}
