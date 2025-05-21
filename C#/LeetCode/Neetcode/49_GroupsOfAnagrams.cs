e6e1bd0077800e2f8c5c0ff165c735ac6013a5eb
Milan Assuied <milan.assuied@unity3d.com>
2025-05-21 13:02:05 -0400

    Group of anagrams

--- C#/LeetCode/Neetcode/49_GroupsOfAnagrams.cs
+++ C#/LeetCode/Neetcode/49_GroupsOfAnagrams.cs
@@ -10,6 +10,31 @@ public class GroupsOfAnagrams
     public IList<IList<string>> Solution(string[] strs)
     {
         var results = new List<IList<string>>();
+        var groups = new Dictionary<string, IList<string>>();
+
+        foreach (var str in strs)
+        {
+            var tracker = new char[26];
+
+            foreach (var t in str)
+            {
+                tracker[t - 'a']++;
+            }
+            var strKey = new string(tracker);
+
+            if (!groups.TryGetValue(strKey, out var value))
+            {
+                value = new List<string>();
+                value.Add(str);
+                groups.Add(strKey, value);
+                results.Add(value);
+                continue;
+            }
+
+            groups[strKey].Add(str);
+        }
+        
         return results;
     }
+
 }
