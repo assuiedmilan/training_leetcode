8680b349c8e648731dd67e4f8139fae85c568928
Milan Assuied <milan.assuied@unity3d.com>
2025-05-28 15:19:25 -0400

    MinimumWindowSubstring

---
+++ C#/LeetCode/Neetcode/76_MinimumWindowSubstring.cs
@@ -0,0 +1,54 @@
+﻿namespace LeetCode.Neetcode;
+
+/* https://leetcode.com/problems/minimum-window-substring/description/ */
+public class MinimumWindowSubstring
+{
+    public string Solution(string s, string t)
+    {
+        if (t.Length > s.Length) return "";
+
+        var referenceTable = new int[58];
+        var trackingTable = new int[58];
+        var referenceSum = t.Length;
+        var trackingSum = 0;
+        var minLen = int.MaxValue;
+        var startIndex = 0;
+        
+        foreach (var c in t)
+        {
+            referenceTable[c - 'A'] += 1;
+        }
+
+        var left = 0;
+        for (var right = 0; right < s.Length; right++)
+        {
+            var c = s[right];
+            var cIndex = c - 'A';
+            trackingTable[cIndex] += 1;
+
+            if (trackingTable[cIndex] <= referenceTable[cIndex]) trackingSum++;
+
+            if (trackingSum == referenceSum)
+            {
+                var leftChar = s[left];
+                var leftCharIndex = leftChar - 'A';
+                while (left < right && (referenceTable[leftCharIndex] == 0 || trackingTable[leftCharIndex] > referenceTable[leftCharIndex]))
+                {
+                    trackingTable[leftCharIndex] -= 1;
+                    left++;
+                    leftChar = s[left];
+                    leftCharIndex = leftChar - 'A';
+                }
+                
+                var len = right - left + 1;
+                if (minLen > len)
+                {
+                    startIndex = left;
+                    minLen = len;
+                }
+            }
+        }
+
+        return trackingSum == referenceSum ? s.Substring(startIndex, minLen) : "";
+    }
+} 
---
+++ C#/TestLeetCode/76_TestMinimumWindowSubstring.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using LeetCode.Neetcode;
+using NUnit.Framework;
+
+namespace TestLeetCode;
+
+public class TestMinimumWindowSubstring
+{
+    static IEnumerable<TestCaseData> TestCases()
+    {
+        yield return new TestCaseData("ADOBECODEBANC", "ABC").Returns("BANC").SetName("BasicExample");
+        yield return new TestCaseData("a", "a").Returns("a").SetName("SingleCharMatch");
+        yield return new TestCaseData("a", "aa").Returns("").SetName("NotEnoughChars");
+        yield return new TestCaseData("ab", "b").Returns("b").SetName("SecondCharMatch");
+        yield return new TestCaseData("ab", "a").Returns("a").SetName("FirstCharMatch");
+        yield return new TestCaseData("ab", "c").Returns("").SetName("NoMatch");
+        yield return new TestCaseData("abcabdebac", "cda").Returns("cabd").SetName("MultiplePossibleWindows");
+        yield return new TestCaseData("a", "b").Returns("").SetName("TestCase192");
+        yield return new TestCaseData("cabwefgewcwaefgcf", "cae").Returns("cwae").SetName("TestCase208");
+    }
+
+    [Test, TestCaseSource(nameof(TestCases))]
+    public string TestSolution(string s, string t)
+    {
+        var testObject = new MinimumWindowSubstring();
+        return testObject.Solution(s, t);
+    }
+}
