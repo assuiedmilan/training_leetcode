12b4b29d87987b28e913f1eaf785a8040b221875
Milan Assuied <milan.assuied@unity3d.com>
2025-06-02 13:44:26 -0400

    FirstMissingPositiveInteger

---
+++ C#/LeetCode/Neetcode/41_FirstMissingPositiveInteger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LeetCode.Neetcode;
+
+/* https://leetcode.com/problems/first-missing-positive/description/ */
+public class FirstMissingPositiveInteger
+{
+    public int Solution(int[] nums)
+    {
+        for (var i = 0; i < nums.Length; i++)
+        {
+            while (nums[i] > 0 && nums[i] <= nums.Length && nums[nums[i] - 1] != nums[i])
+            {
+                (nums[nums[i] - 1], nums[i]) = (nums[i], nums[nums[i] - 1]);
+            }
+        }
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != i + 1) return i + 1;
+        }
+
+        return nums.Length + 1;
+
+    }
+}
---
+++ C#/TestLeetCode/41_TestFirstMissingPositiveInteger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using LeetCode.Neetcode;
+using NUnit.Framework;
+
+namespace TestLeetCode;
+
+public class TestFirstMissingPositiveInteger
+{
+    static IEnumerable<TestCaseData> TestCases()
+    {
+        yield return new TestCaseData((int[]) [1, 2, 0]).Returns(3).SetName("MissingThree");
+        yield return new TestCaseData((int[]) [3, 4, -1, 1]).Returns(2).SetName("MissingTwo");
+        yield return new TestCaseData((int[]) [7, 8, 9, 11, 12]).Returns(1).SetName("MissingOne");
+        yield return new TestCaseData((int[]) [1]).Returns(2).SetName("SingleOne");
+        yield return new TestCaseData((int[]) [2]).Returns(1).SetName("SingleTwo");
+        yield return new TestCaseData((int[]) []).Returns(1).SetName("EmptyArray");
+        yield return new TestCaseData((int[]) [1, 1]).Returns(2).SetName("DuplicateOnes");
+        yield return new TestCaseData((int[]) [2, 2, 2, 2]).Returns(1).SetName("AllTwos");
+        yield return new TestCaseData((int[]) [1, 2, 3, 4, 5]).Returns(6).SetName("ConsecutiveFromOne");
+        yield return new TestCaseData((int[]) [-1, -2, -3]).Returns(1).SetName("AllNegatives");
+        yield return new TestCaseData((int[]) [4, 5, -1, 2, 1]).Returns(3).SetName("Custom");
+    }
+
+    [Test, TestCaseSource(nameof(TestCases))]
+    public int TestSolution(int[] nums)
+    {
+        var testObject = new FirstMissingPositiveInteger();
+        return testObject.Solution(nums);
+    }
+}
