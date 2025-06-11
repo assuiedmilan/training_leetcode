8d6d881d48646abe05424e4e87624751ce877480
Genevieve Dufour <genevieve.dufour@unity3d.com>
2025-06-11 10:04:23 -0400

    tree

---
+++ LeetCode/Gen/InvertBinaryTree226.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Gen;
+
+// 226. Invert Binary Tree
+
+public class InvertBinaryTree226
+{
+    public TreeNode InvertTree(TreeNode root)
+    {
+        if (root == null) return null;
+        if(root.left == null && root.right == null) return root;
+
+        var temp = root.left;
+        root.left = InvertTree(root.right);
+        root.right = InvertTree(temp);
+
+        return root;
+    }
+}
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
---
+++ LeetCode/Gen/MaximumDepthBinaryTree104.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Gen;
+
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+// 104. Maximum Depth of Binary Tree
+
+public class MaximumDepthBinaryTree104
+{
+    public int MaxDepth(TreeNode root)
+    {
+        var maxDepth = 0;
+        var currentDepth = 0;
+        while (root.left != null)
+        {
+            currentDepth += 1;
+        }
+
+        return 0;
+    }
+}
