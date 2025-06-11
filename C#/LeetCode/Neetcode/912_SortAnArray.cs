0d8ee1def527d69d11a8ebe9b544f9978b20c49b
Milan Assuied <milan.assuied@unity3d.com>
2025-06-11 10:04:06 -0400

    Sort Array using heap sort

---
+++ C#/LeetCode/Neetcode/912_SortAnArray.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Neetcode;
+
+/* https://leetcode.com/problems/sort-an-array/ */
+public class SortArray
+{
+    public int[] Solution(int[] nums)
+    {
+        MaxHeap(nums);
+
+        for (var i = nums.Length - 1; i > 0; i--) {
+            (nums[0], nums[i]) = (nums[i], nums[0]);
+            MaxHeap(nums[..^(nums.Length - i)], 0);
+        }
+        
+        return nums;
+    }
+
+    static void MaxHeap(int[] arr)
+    {
+        var lastNonleafNode = arr.Length / 2 - 1;
+        
+        for(var i = lastNonleafNode; i > -1; i--) MaxHeap(arr, i);
+    }
+
+    static void MaxHeap(int[] arr, int root)
+    {
+        while (true)
+        {
+            var largestNodeIndex = root;
+            var leftLeafIndex = 2 * root + 1;
+            var rightLeafIndex = leftLeafIndex + 1;
+
+            if (leftLeafIndex < arr.Length && arr[leftLeafIndex] > arr[largestNodeIndex]) largestNodeIndex = leftLeafIndex;
+            if (rightLeafIndex < arr.Length && arr[rightLeafIndex] > arr[largestNodeIndex]) largestNodeIndex = rightLeafIndex;
+
+            if (largestNodeIndex != root)
+            {
+                (arr[largestNodeIndex], arr[root]) = (arr[root], arr[largestNodeIndex]);
+                root = largestNodeIndex;
+                continue;
+            }
+
+            break;
+        }
+    }
+}
---
+++ C#/TestLeetCode/912_TestSortAnArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using LeetCode.Neetcode;
+using NUnit.Framework;
+
+namespace TestLeetCode;
+
+public class TestSortArray
+{
+    static IEnumerable<TestCaseData> TestCases()
+    {
+        yield return new TestCaseData((int[]) [5, 2, 3, 1]).Returns((int[]) [1, 2, 3, 5]).SetName("UnsortedArray");
+        yield return new TestCaseData((int[]) [5, 1, 1, 2, 0, 0]).Returns((int[]) [0, 0, 1, 1, 2, 5]).SetName("ArrayWithDuplicates");
+        yield return new TestCaseData((int[]) []).Returns((int[]) []).SetName("EmptyArray");
+        yield return new TestCaseData((int[]) [1]).Returns((int[]) [1]).SetName("SingleElementArray");
+        yield return new TestCaseData((int[]) [int.MaxValue, int.MinValue, 0]).Returns((int[]) [int.MinValue, 0, int.MaxValue]).SetName("ArrayWithExtremeValues");
+        yield return new TestCaseData((int[]) [-1, -2, -3, -4, -5]).Returns((int[]) [-5, -4, -3, -2, -1]).SetName("NegativeNumbers");
+    }
+
+    [Test, TestCaseSource(nameof(TestCases))]
+    public int[] TestSolution(int[] nums)
+    {
+        var testObject = new SortArray();
+        return testObject.Solution(nums);
+    }
+}
