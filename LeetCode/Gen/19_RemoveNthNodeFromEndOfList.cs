c8cdc14f52b81918a5fdf32334a32dde85829d4f
Genevieve Dufour <genevieve.dufour@unity3d.com>
2025-07-23 11:48:22 -0400

    19. Remove Nth Node From End of List

---
+++ LeetCode/Gen/19_RemoveNthNodeFromEndOfList.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.Gen;
+
+public class RemoveNthNodeFromEndOfList19 {
+    public ListNode RemoveNthFromEnd(ListNode head, int n) {
+        if (head == null || head.next == null)
+            return null;
+
+        ListNode fast = head;
+        ListNode slow = head;
+        int f = 0;
+        int s = 0;
+
+        while (fast != null)
+        {
+            fast = fast.next;
+            f++;
+
+            if (f - s > n+1)
+            {
+                s++;
+                slow = slow.next;
+            }
+        }
+
+        if(f == n)
+            return slow.next;
+
+        slow.next = slow.next.next;
+        return head;
+    }
+}
