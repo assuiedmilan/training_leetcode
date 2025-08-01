d81f764f38d12007db59d7d3c919e6782942481e
Genevieve Dufour <genevieve.dufour@unity3d.com>
2025-08-01 11:45:47 -0400

    138. Copy List with Random Pointer

---
+++ LeetCode/Gen/138_CopyListWithRandomPointer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Gen;
+
+public class CopyListWithRandomPointer138 {
+
+    public Node CopyRandomList(Node head)
+    {
+        if (head == null) return null;
+
+        var mapOldListToNewList = new Dictionary<Node, Node>();
+
+        var currentOldNode = head;
+
+        while (currentOldNode != null)
+        {
+            mapOldListToNewList.Add(currentOldNode, new Node(currentOldNode.val));
+            currentOldNode = currentOldNode.next;
+        }
+
+        currentOldNode = head;
+        while (currentOldNode != null)
+        {
+            if (currentOldNode.next == null)
+            {
+                mapOldListToNewList[currentOldNode].next = null;
+            }
+            else
+            {
+                mapOldListToNewList[currentOldNode].next = mapOldListToNewList[currentOldNode.next];
+            }
+
+            if (currentOldNode.random == null)
+            {
+                mapOldListToNewList[currentOldNode].random = null;
+            }
+            else
+            {
+                mapOldListToNewList[currentOldNode].random = mapOldListToNewList[currentOldNode.random];
+            }
+
+            currentOldNode = currentOldNode.next;
+        }
+
+        mapOldListToNewList.TryGetValue(head, out var newHeadNode);
+
+        return newHeadNode;
+    }
+}
+
+/*
+ // Pour les tests
+ var n1 = new Node(7);
+        var n2 = new Node(13);
+        var n3 = new Node(11);
+        var n4 = new Node(10);
+        var n5 = new Node(1);
+        n1.next = n2;
+        n2.next = n3;
+        n2.random = n1;
+        n3.next = n4;
+        n3.random = n5;
+        n4.next = n5;
+        n4.random = n3;
+        n5.random = n1;
+ */
--- LeetCode/Gen/206_ReverseLinkedList.cs
+++ LeetCode/Gen/206_ReverseLinkedList.cs
@@ -54,6 +54,18 @@ public class ListNode
     }
 }
 
+public class Node {
+    public int val;
+    public Node next;
+    public Node random;
+
+    public Node(int _val) {
+        val = _val;
+        next = null;
+        random = null;
+    }
+}
+
 public static class ListNodeUtilities
 {
     public static ListNode MakeList(int[] values, int pos)
